c5592a3ff5e3057f78e45c37f335587bfd7f67f9
vyuwono <vinzz_y@yahoo.com>
2015-05-26 11:31:14 +1000

    add current temperature to data by location_id table

--- forecast/app/controllers/weathers_controller.rb
+++ forecast/app/controllers/weathers_controller.rb
@@ -15,6 +15,23 @@ class WeathersController < ApplicationController
       @name = params[:by_location]
       @locations = Location.where("location_id LIKE ?", "%#{params[:by_location]}%")
       @descriptions = Description.all
+
+      #find current temperature
+      now = Time.now
+      min_difference = 999999
+      @current_temp = nil
+      @locations.each do |location| 
+        @descriptions.each do |forecast|
+          if forecast.location_id == location.id
+            if (now - forecast.datetime) < 1800 && (now-forecast.datetime) < min_difference
+              @current_temp = forecast.temp.to_s
+              @min_difference = now - forecast.datetime
+            end
+          end
+        end
+      end
+
+
     elsif params[:by_postcode]
       @result = "by_postcode"
       @name = params[:by_postcode].to_i
